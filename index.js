aea6c6caec6e34cd106914c748823c8519799013
hrmrakib <hrmrakibs@gmail.com>
2024-07-16 10:41:41 +0600

    user register route maked and pending for approval

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dmwxvyo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -17,6 +29,38 @@ async function run() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
+
+    // database name
+    const database = client.db("mobileFinanceSystem");
+    // collections
+    const userCollection = database.collection("users");
+
+    // register a user
+    app.post("/register", async (req, res) => {
+      const { name, pin, mobileNumber, email } = req.body;
+
+      console.log(name, pin, mobileNumber, email);
+      const existEmail = await userCollection.findOne({ email });
+      const existMobileNumber = await userCollection.findOne({ mobileNumber });
+
+      if (existEmail || existMobileNumber) {
+        return res.send("Exist the user!");
+      }
+
+      const hashedPin = await bcrypt.hash(pin, 10);
+
+      const newUser = {
+        name,
+        pin: hashedPin,
+        mobileNumber,
+        email,
+        status: "pending",
+        balance: 0,
+      };
+
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
